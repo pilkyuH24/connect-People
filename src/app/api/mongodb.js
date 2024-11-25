55604fd8c9834a8e096f06009cf9bcc287c2508f
Pilkyu Han <pil331@berkeley.edu>
2024-11-24 18:37:39 -0800

    vercel env issue

--- src/app/api/mongodb.js
+++ src/app/api/mongodb.js
@@ -3,16 +3,16 @@ import { MongoClient } from 'mongodb';
 const uri = process.env.MONGODB_URI;
 const options = {};
 
-let client;
 let clientPromise;
 
+
 if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your MongoDB URI to .env.local');
+  throw new Error('process.env.MONGODB_URI error' + ' MONGODB_URI:', process.env.MONGODB_URI);
 }
 
 if (process.env.NODE_ENV === 'development') {
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+    const client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
